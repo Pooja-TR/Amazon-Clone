e2fc049f703c347610f72707531e6c4789ace0a7
Pooja-TR <trpooja978@gmail.com>
2020-11-18 15:27:27 +0530

    4.Adding React-router-dom

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
-import './App.css';
-import Header from './Header'
-import Home from './Home'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import "./App.css";
+import Header from "./Header";
+import Home from "./Home";
 
 function App() {
   return (
-    <div className="app">
-      <Header />
-      <Home />
-      { /* Home  */}
-    </div>
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path="/checkout">
+            <Header />
+            <h1>I AM AT CHECKOUT GO BACK AS OF NOW </h1>
+          </Route>
+
+          <Route path="/">
+            <Header />
+            <Home />
+          </Route>
+        </Switch>
+        {/* Home  */}
+      </div>
+    </Router>
   );
 }
 
